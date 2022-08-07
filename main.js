c4d57ed1496f8dd901c50a035ab3b61aa986320a
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-08-07 21:50:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'HALO @user 👋\n SELAMAT DATANG DI  @subject \n❋𝗝𝗔𝗦𝗔 𝗥𝗨𝗡❋\n-𝚃𝚒𝚗𝚐𝚐𝚊𝚕 𝚂𝚌𝚊𝚗\n-𝟸𝟺𝙹𝚊𝚖 𝚋𝚘𝚝 𝚊𝚔𝚝𝚒𝚏\n𝙿𝚊𝚢𝚖𝚎𝚗𝚝 : 𝙳𝙰𝙽𝙰\n𝙼𝚒𝚗𝚊𝚝 ? wa.me/6287898307350' 
+  conn.welcome = 'HALO @user 👋\n SELAMAT DATANG DI  @subject' 
   conn.bye = 'YAHHH @user TELAH KELUAR DARI @subject'
   conn.spromote = '@user sekarang admin!'
   conn.sdemote = '@user sekarang bukan admin!'
